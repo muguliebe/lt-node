f7a1e195434892bf0fdc5612f7cfb261ec252353
zany <muguliebe@gmail.com>
2017-06-25 21:44:19 +0900

    change port

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 var http = require('http');
-
+var port = process.env.PORT || 8080
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
-}).listen(80);
+}).listen(port);
