a76b3268e42c4dc70efcfdad09429f1052bf80f1
zany <muguliebe@gamil.com>
2017-06-26 21:00:56 +0900

    add users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const fetch = require('node-fetch')
 
 router.get('/users', (req,res) => {
-  res.render('users', {
-  })
+
+  fetch('https://api.github.com/users')
+    .then(function(res) {
+        return res.json()
+    }).then(function(json) {
+      res.render('users', {
+        users: json
+      })
+    }).catch(err => {
+      console.error(err)
+    })
+
 })
 
 module.exports = router
