038a272538822d832e853b380371db9739c05d67
zany <muguliebe@gamil.com>
2017-06-26 17:34:14 +0900

    split url

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ import DateUtils from './modules/date'
 
 console.log('start')
 http.createServer(function (req, res) {
+  if (req.method === 'GET' && req.url === '/') {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!!!' + DateUtils.myDate());
+  } else if (req.method === 'GET' && req.url === '/test') {
+    res.statusCode = 200
+    res.end('good job')
+  } else {
+    res.statusCode = 404
+    res.end()
+  }
 }).listen(port);
