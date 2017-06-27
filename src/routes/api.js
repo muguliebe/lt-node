e621134e3796aa698bf473fc19f605bf5c419364
zany <muguliebe@gmail.com>
2017-06-27 18:24:21 +0900

    add select mongo

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+import Transaction from '../model/transaction'
 
 router.get('/api', (req,res) => {
   res.json(`alive with ${process.env.NODE_ENV}`)
@@ -8,6 +9,15 @@ router.get('/api', (req,res) => {
 router.get('/api/test', (req, res) => {
   console.log('/test start')
   res.json(`good job`)
+})
+
+router.get('/api/transactions', (req, res) => {
+  Transaction.find().exec()
+    .then(result => {
+      res.json(result)
+    }).catch(err => {
+      console.error('transaction find err:', err)
+    })
 })
 
 // export default router
